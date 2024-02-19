70cbf011f72988e008a39a6b9fec2f20a646c84c
JosuaMnl <josuamnalu@gmail.com>
2024-02-19 15:04:54 +0700

    CodeWars Day 21: Multiples of 3 or 5

---
+++ 21-multiplesOf3or5.kt
@@ -0,0 +1,14 @@
+// Day 21/366
+// 
+
+// My Answers
+fun solution(number: Int) =(1..number-1).asSequence().filter { it % 3 == 0 || it % 5 == 0 }.sum()
+
+// Best Practices
+// fun solution(number: Int): Int = (3 until number step 3).union((5 until number step 5)).sum()
+
+fun main() {
+    println(solution(10))
+    println(solution(20))
+    println(solution(200))
+}
