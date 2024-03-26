6601e6c32945aa1a3a0b4d12e65c3b652edfdb8d
JosuaMnl <josuamnalu@gmail.com>
2024-03-26 23:37:53 +0700

    CodeWars Day 63: Sum of Angles

---
+++ 63-sumOfAngles.kt
@@ -0,0 +1,10 @@
+// Day 63/366
+// https://www.codewars.com/kata/5a03b3f6a1c9040084001765
+
+// My Answers
+fun angle(n: Int) = (n-2)*180
+
+fun main() {
+    println(angle(3))
+    println(angle(4))
+}
