f1392566543c6f1a12b1af86d98c1eb709f89e2d
JosuaMnl <josuamnalu@gmail.com>
2024-04-01 22:13:56 +0700

    CodeWars Day 68: Alphabetical Addition

---
+++ 68-alphabeticalAddition.kt
@@ -0,0 +1,17 @@
+// Day 68/366
+// https://www.codewars.com/kata/5d50e3914861a500121e1958
+
+// My Answers
+fun addLetters(arr: List<Char>) : Char {
+    return 'z' - arr.sumOf { 'z' - it } % 26
+}
+
+fun main() {
+    println(addLetters(listOf('a', 'b', 'c'))) // 'f'
+    println(addLetters(listOf('z'))) // 'z'
+    println(addLetters(listOf('a', 'b'))) // 'c'
+    println(addLetters(listOf('c'))) // 'c'
+    println(addLetters(listOf('z', 'a'))) // 'a'
+    println(addLetters(listOf('y', 'c', 'b'))) // 'd'
+    println(addLetters(listOf())) // 'z'
+}
